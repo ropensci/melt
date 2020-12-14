a2d418c3606a9d20ca14854571ee23125136d071
markean <markean625@gmail.com>
2020-12-13 22:52:03 -0500

    update

--- R/RcppExports.R
+++ R/RcppExports.R
@@ -41,16 +41,38 @@ test2sample_cpp <- function(x, y, b = .9, maxit = 1000L, abstol = 1e-8) {
 #' @param x a vector of data for one  group.
 #' @param y a vector of data for the other  group.
 #' @param b a momentum parameter for minimization. Defaults to .1.
+#' @param alpha an optional step size. Defaults to 1.
 #' @param maxit an optional value for the maximum number of iterations. Defaults to 1000.
 #' @param abstol an optional value for the absolute convergence tolerance. Defaults to 1e-8.
 #'
 #' @examples
 #' x <- rnorm(100)
 #' y <- rnorm(100)
-#' test2sample2(x, y)
+#' test2sample2_cpp(x, y)
 #'
 #' @export
 test2sample2_cpp <- function(x, y, b = .9, alpha = 1, maxit = 1000L, abstol = 1e-8) {
     .Call(`_bayesELcpp_test2sample2_cpp`, x, y, b, alpha, maxit, abstol)
+}
+
+#' Two sample test for equal mean
+#'
+#' Two sample test for equal mean
+#'
+#' @param x a vector of data for one  group.
+#' @param y a vector of data for the other  group.
+#' @param b a momentum parameter for minimization. Defaults to .1.
+#' @param alpha an optional step size. Defaults to 1.
+#' @param maxit an optional value for the maximum number of iterations. Defaults to 1000.
+#' @param abstol an optional value for the absolute convergence tolerance. Defaults to 1e-8.
+#'
+#' @examples
+#' x <- rnorm(100)
+#' y <- rnorm(100)
+#' test2sample777_cpp(x, y)
+#'
+#' @export
+test2sample777_cpp <- function(x, y, b = .9, alpha = 1, maxit = 1000L, abstol = 1e-8) {
+    .Call(`_bayesELcpp_test2sample777_cpp`, x, y, b, alpha, maxit, abstol)
 }
 
--- src/utils.cpp
+++ src/utils.cpp
@@ -165,7 +165,7 @@ List test2sample_cpp(arma::vec x, arma::vec y, double b = .9,
     ly = elMeancpp({par}, y)["lambda"];
     // gradient
     grad = {sum(dplogcpp(1 + lx * (x - par), 1 / nx)) * (-lx),
-      sum(dplogcpp(1 + ly * (y - par), 1 / ny)) * (-ly)};
+            sum(dplogcpp(1 + ly * (y - par), 1 / ny)) * (-ly)};
     // direction
     d = dfp1dcpp(grad);
     // direction change reverts momentum
@@ -213,13 +213,14 @@ List test2sample_cpp(arma::vec x, arma::vec y, double b = .9,
 //' @param x a vector of data for one  group.
 //' @param y a vector of data for the other  group.
 //' @param b a momentum parameter for minimization. Defaults to .1.
+//' @param alpha an optional step size. Defaults to 1.
 //' @param maxit an optional value for the maximum number of iterations. Defaults to 1000.
 //' @param abstol an optional value for the absolute convergence tolerance. Defaults to 1e-8.
 //'
 //' @examples
 //' x <- rnorm(100)
 //' y <- rnorm(100)
-//' test2sample2(x, y)
+//' test2sample2_cpp(x, y)
 //'
 //' @export
 // [[Rcpp::export]]
@@ -255,6 +256,107 @@ List test2sample2_cpp(arma::vec x, arma::vec y, double b = .9, double alpha = 1,
     // lambda update
     lx = elMeancpp({par}, x)["lambda"];
     ly = elMeancpp({par}, y)["lambda"];
+    // gradient
+    grad = {sum(dplogcpp(1 + lx * (x - par), 1 / nx)) * (-lx) / N,
+            sum(dplogcpp(1 + ly * (y - par), 1 / ny)) * (-ly) / N};
+    // direction
+    d = dfp1dcpp(grad);
+    // direction change reverts momentum
+    if (sign(d) != sign(v)) {
+      v = 0; alpha = alpha / 2;
+    }
+    // lb, ub update
+    if (sign(d) > 0) {
+      lb = par;
+    } else {
+      ub = par;
+    }
+    // convergence check & parameter update
+    if ((std::abs(d * sum(grad)) < abstol || ub - lb < abstol) && iterations > 0) {
+      convergence = 1;
+    } else {
+      iterations++;
+      // step halving to satisfy convex hull constraint
+      v = b * v + d;
+      while (par + alpha * v <= lb || par + alpha * v >= ub) {
+        alpha = alpha / 2;
+      }
+      par = par + alpha * v;
+      if(iterations == maxit) {
+        break;
+      }
+    }
+  }
+
+  // result
+  result["par"] = par;
+  result["nlogLR"] = sum(log(1 + (x - par) * lx)) +
+    sum(log(1 + (y - par) * ly));
+  result["iterations"] = iterations;
+  result["convergence"] = convergence;
+  return result;
+}
+
+
+//' Two sample test for equal mean
+//'
+//' Two sample test for equal mean
+//'
+//' @param x a vector of data for one  group.
+//' @param y a vector of data for the other  group.
+//' @param b a momentum parameter for minimization. Defaults to .1.
+//' @param alpha an optional step size. Defaults to 1.
+//' @param maxit an optional value for the maximum number of iterations. Defaults to 1000.
+//' @param abstol an optional value for the absolute convergence tolerance. Defaults to 1e-8.
+//'
+//' @examples
+//' x <- rnorm(100)
+//' y <- rnorm(100)
+//' test2sample777_cpp(x, y)
+//'
+//' @export
+// [[Rcpp::export]]
+List test2sample777_cpp(arma::vec x, arma::vec y, double b = .9, double alpha = 1,
+                        unsigned int maxit = 1000, double abstol = 1e-8) {
+  List result;
+  vec sample_mean = {mean(x), mean(y)};
+  vec ub_vec = {max(x), max(y), max(sample_mean)};
+  vec lb_vec = {min(x), min(y), min(sample_mean)};
+  double ub = min(ub_vec);
+  double lb = max(lb_vec);
+  if(ub <= lb) {
+    result["nlogLR"] = datum::inf;;
+    result["convergence"] = -1;
+    return result;
+  }
+
+  // initialization
+  double par = (lb + ub) / 2;
+  unsigned int nx = x.n_elem;
+  unsigned int ny = y.n_elem;
+  unsigned int N = std::max(nx, ny);
+  unsigned int iterations{};
+  int convergence{};
+  double v{};
+  double lx;
+  double ly;
+  vec grad;
+  double d;
+
+  // linearization
+  double par0 = par;
+  double lx0 = elMeancpp({par}, x)["lambda"];
+  double ly0 = elMeancpp({par}, y)["lambda"];
+  double mx = -sum(1 / pow(1 + lx0 * (x - par), 2)) /
+    sum(pow(x - par, 2) / pow(1 + lx0 * (x - par), 2));
+  double my = -sum(1 / pow(1 + ly0 * (y - par) , 2)) /
+    sum(pow(y - par ,2) / pow(1 + ly0 * (y - par) , 2));
+
+  // minimization
+  while (convergence == 0) {
+    // lambda update
+    lx = mx * (par - par0) + lx0;
+    ly = my * (par - par0) + ly0;
     // gradient
     grad = {sum(dplogcpp(1 + lx * (x - par), 1 / nx)) * (-lx) / N,
             sum(dplogcpp(1 + ly * (y - par), 1 / ny)) * (-ly) / N};
