1a02f27baadf4ebfd454be24b7c7ba55001651aa
markean <markean@protonmail.com>
2021-09-21 19:51:24 -0400

    clothianidin data added.

---
+++ R/data.R
@@ -0,0 +1,15 @@
+#' Clothianidin concentration in maize plants.
+#'
+#' A dataset containing the prices and other attributes of almost 54,000
+#' diamonds.
+#'
+#' @format A data frame with 102 rows and 3 variables:
+#' \describe{
+#'   \item{blk}{blocks constructed from original data. Days post planting-blocks-year.clothianidin}
+#'   \item{trt}{Seed treatments.}
+#'   \item{clo}{log transformed clothianidin concentration in microgram.}
+#' }
+#' @usage data("clothianidin")
+#' @source Alford A, Krupke CH (2017) Translocation of the neonicotinoid seed treatment clothianidin in maize. PLoS ONE 12(3): e0173836.
+#' \url{https://doi.org/10.1371/journal.pone.0173836}
+"clothianidin"
