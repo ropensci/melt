1b9bbebdbd7c6bc58b34f83f8794bafa7b449d9d
markean <markean@protonmail.com>
2022-07-14 16:53:36 -0400

    documentation update

--- R/summary-methods.R
+++ R/summary-methods.R
@@ -1,5 +1,5 @@
-#' @describeIn summary Summarizes the results of the overall test and the
-#'   tests for each parameter.
+#' @describeIn summary Summarizes the results of the overall model test and the
+#'   significance tests for coefficients.
 #' @importFrom stats pchisq
 #' @srrstats {RE4.18} `summary` method is applicable to a `LM` object returned
 #'   by `el_lm()` or `el_glm()`.
